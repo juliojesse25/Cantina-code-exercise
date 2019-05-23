258f916551c6eb99b37850eda4352951c9cd847d
juliojesse25 <juliojesse25@gmail.com>
2019-05-22 21:50:04 -0400

    Add function to check if userInput is equal to a class

--- solution/lib/code-exercise.js
+++ solution/lib/code-exercise.js
@@ -3,7 +3,8 @@
 const fs = require('fs')
 const readline = require('readline')
 
-
+const answer = []
+let count = 1
 const parseJson = function(inFile) {
   const rl = readline.createInterface({
     input: process.stdin,
@@ -20,16 +21,74 @@ const parseJson = function(inFile) {
 
     try {
       const pojo = JSON.parse(fileContents)
-      console.log(pojo)
-      }
-      rl.question(">>Enter a selector ", function(answer){
-        console.log("Hello " + answer)
+
+      // for (var prop in pojo) {
+      //   string1 += object1[property1];
+      // }
+      rl.question(">>Enter a selector ", function(userInput){
+        console.log("Hello " + userInput)
+
+        // console.log(pojo)
+        reader(pojo['subviews'], userInput)
+        printAnswers(answer)
+        console.log(`Total number of DOM nodes ${answer.length} \n\n`)
+
         rl.close()
       })
-    } catch(err) {
+      }catch(err) {
       console.error(err)
-    }
+      }
   })
 }
+
+
+function reader(parentSubview, userInput){
+  // console.log(parentSubview[0])
+  if(parentSubview.length){
+    //const numberOfSubviews = parentSubview.length
+    //console.log(numberOfSubviews)
+    parentSubview.map(view =>{
+      if (view['class'] === userInput ){
+        answer.push(view)
+        //console.log(view.class)
+        console.log(view)
+        //console.log(view['subviews'])
+      }
+      if(view['subviews']){
+        reader(view['subviews'], userInput)
+      } else if(view['contentView']) {
+
+        if(view['contentView']['subviews'].length){
+          const contentSubview = view['contentView']['subviews']
+          contentSubview.map(element =>{
+            if (element['class'] === userInput ){
+              answer.push(element)
+            }
+            if(element['control']){
+              if (element['control']['class'] === userInput ){
+                answer.push(element)
+              }
+            }
+          })
+        }
+
+      }
+    })
+  }
+}
+
+function printAnswers(answers){
+  answers.map(item=>{
+    let propValue;
+    for(let propName in item) {
+        propValue = item[propName]
+        console.log(propName,propValue);
+    }
+    console.log(`Node Number: ${count} \n \n \n`)
+    count = count + 1
+  })
+}
+
+
 
 module.exports = parseJson
